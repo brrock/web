d5eb7f927829c082571fcb8d94e98695ba237e51
Benjy <brduck@duck.com>
2023-03-03 07:37:19 +0000

    starting changes

---
+++ .eleventy.js
@@ -0,0 +1,16 @@
+const { DateTime } = require("luxon")
+module.exports = function(eleventyConfig) {
+  eleventyConfig.addPassthroughCopy('./src/style.css');
+  eleventyConfig.addPassthroughCopy('./src/assets');
+  eleventyConfig.addPassthroughCopy('./src/admin/config.yml');    
+
+  eleventyConfig.addFilter("postDate", (dateObj) => {
+    return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
+  });
+        return {
+          dir: {
+            input: "src",
+            output: "public"
+          }
+        }
+      };
