67f375f06499800b7eefe590a915f98f8ef6bb01
brrock <123623302+brrock@users.noreply.github.com>
2024-05-02 07:22:07 +0100

    bug fixes

--- .eleventy.js
+++ .eleventy.js
@@ -3,7 +3,8 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy('./src/style.css');
   eleventyConfig.addPassthroughCopy('./src/assets');
   eleventyConfig.addPassthroughCopy('./src/admin/config.yml');    
-  eleventyConfig.addPassthroughCopy('./src/robots.txt');    
+  eleventyConfig.addPassthroughCopy('./src/robots.txt'); 
+  eleventyConfig.addPassthroughCopy('./src/output/style.css'); 
   eleventyConfig.addFilter("postDate", (dateObj) => {
     return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
   });
